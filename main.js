8c044c557089480a22d8f411a7eeba17f69e77f0
Arjun8485 <64953034+Arjun8485@users.noreply.github.com>
2021-08-24 01:33:04 +0530

    Update main.js

--- main.js
+++ main.js
@@ -167,3 +167,17 @@ function returnRetrievedMessages(doc) {
     </div>
     `;
 }
+
+
+
+Notification.requestPermission(function(status) {
+    console.log('Notification permission status:', status);
+});
+
+function displayNotification() {
+  if (Notification.permission == 'granted') {
+    navigator.serviceWorker.getRegistration().then(function(reg) {
+      reg.showNotification('Hello world!');
+    });
+  }
+}
