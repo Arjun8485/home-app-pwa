cd6e8daa01b5d5236a50fc28d7c5bf93e2f7de7d
Arjun8485 <64953034+Arjun8485@users.noreply.github.com>
2021-08-24 12:07:18 +0530

    Update main.js

--- main.js
+++ main.js
@@ -103,10 +103,16 @@ var uiConfig = {
         return afterSignIn(authResult);
         },
     },
-    signInFlow: 'redirect',
+    signInFlow: 'popup',
     signInSuccessUrl: '<url-to-redirect-to-on-success>',
     signInOptions: [
-        firebase.auth.GoogleAuthProvider.PROVIDER_ID
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+          firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+          firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+          firebase.auth.GithubAuthProvider.PROVIDER_ID,
+          firebase.auth.EmailAuthProvider.PROVIDER_ID,
+          firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+          firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
     ]
 };
 
